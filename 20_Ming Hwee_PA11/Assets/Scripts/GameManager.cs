8a86bd561e4be2b8abdce5341e6dcdb24e8a6066
Orange00h <64114714+Orange00h@users.noreply.github.com>
2020-06-18 17:33:16 +0800

    PA11 done
    Done

---
+++ 20_Ming Hwee_PA11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager thisManager;
+
+    public Text Score;
+
+    public float ScoreNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+        ScoreNum++;
+        Score.text = "Score : " + ScoreNum;
+    }
+}
---
+++ 20_Ming Hwee_PA11/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        float yLock = Mathf.Clamp(transform.position.y, -3.9f, 3.9f);
+
+        transform.position = new Vector3(transform.position.x, yLock, transform.position.z);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
